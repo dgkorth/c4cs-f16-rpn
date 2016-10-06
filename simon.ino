6e24ee58cd66fa062945eeeac384264be725350c
Daniel Korth <dgkorth@Daniels-MBP-2.lan>
2016-10-05 22:37:00 -0400

    fixed light and switch statement in gamepluh

--- simon.ino
+++ simon.ino
@@ -5,35 +5,46 @@
 #define B2 4
 #define B3 5
 #define L0 8
-#define L1 8
-#define L2 8
-#define L3 8
+#define L1 9
+#define L2 10
+#define L3 11
 #define LCD 12
 #define SPKR 13
+
 // game state
 enum game{INSTRUCTION,PROMPT,WIN,LOSE};
 game state;
+
 // buttonSequence of buttons to be pressed
 const int buttonSequence[] = {B1,B3,B2,B0};
 const int ledSequence[] = {L1,L3,L2,L0};
+
 // game levels
 int level = 0;
+
 //varibles for PROMPT 
 int usrB;
 
 
 void setup() {
+// initialize serial monitor
+Serial.begin(9600);
+delay(1000);
+
 // initialize LED pins
 pinMode(L0,OUTPUT);
 pinMode(L1,OUTPUT);
 pinMode(L2,OUTPUT);
 pinMode(L3,OUTPUT);
+
 // initialize buttons
 pinMode(B0,INPUT_PULLUP);
 pinMode(B1,INPUT_PULLUP);
 pinMode(B2,INPUT_PULLUP);
 pinMode(B3,INPUT_PULLUP);
+
 // test and reset LEDs
+Serial.println("test leds");
 digitalWrite(L0,HIGH);
 digitalWrite(L1,HIGH);
 digitalWrite(L2,HIGH);
@@ -43,9 +54,11 @@ digitalWrite(L0,LOW);
 digitalWrite(L1,LOW);
 digitalWrite(L2,LOW);
 digitalWrite(L3,LOW);
-// initialize serial monitor
-Serial.begin(9600);
-delay(1000);
+
+//test speaker
+Serial.println("test speaker");
+tone(SPKR,NOTE_C4,1000);
+
 //set game to start
 state = INSTRUCTION;
 level = 0;
@@ -67,7 +80,7 @@ void gameplay() {
     // tell player sequence to be entered
     case INSTRUCTION:
         Serial.println("instruction");
-        for (int i = 0; i < level + 1; i++) {
+        for (int i = 0; i < (level + 1); i++) {
             light(ledSequence[i],500);
         }
         state = PROMPT;
@@ -107,7 +120,8 @@ void gameplay() {
         lightPattern(2,2);
         lightPattern(0,3);
         level++;
-        state = PROMPT;
+        delay(3000);
+        state = INSTRUCTION;
         Serial.println("prompt");
         break;
     // tell player they lost and reset entire game
@@ -116,8 +130,8 @@ void gameplay() {
         lightPattern(2,2);
         lightPattern(1,3);
         level = 0;
-        state = PROMPT;
-        Serial.println("prompt");
+        delay(3000);
+        state = INSTRUCTION;
         break;
     }
 }
@@ -125,20 +139,20 @@ void gameplay() {
 void light(int led, int duration) {
     Serial.println("light");
     int note;
-        if (L0) {
+        if (led == L0) {
             note = NOTE_C3; 
         }
-        if (L1) {
+        if (led == L1) {
             note = NOTE_C4; 
         }
-        if (L2) {
+        if (led == L2) {
             note = NOTE_C5; 
         }
-        if (L3) {
+        if (led == L3) {
             note = NOTE_C6; 
         }
     digitalWrite(led,HIGH);
-    tone(8,note,duration);
+    tone(SPKR,note,duration);
     delay(duration);
     digitalWrite(led,LOW);
     return;
@@ -146,28 +160,29 @@ void light(int led, int duration) {
 
 void lightPattern(int pattern, int reps) {
     Serial.println("lightPattern");
+    int dur = 100;
     if (pattern == 0) {
         for (int i = 0; i < reps; i++) {
-            light(L0,300);
-            light(L1,300);
-            light(L2,300);
-            light(L3,300);
+            light(L0,dur);
+            light(L1,dur);
+            light(L2,dur);
+            light(L3,dur);
         }
     }
     if (pattern == 1) {
         for (int i = 0; i < reps; i++) {
-            light(L3,300);
-            light(L2,300);
-            light(L1,300);
-            light(L0,300);
-        } 
+            light(L3,dur);
+            light(L2,dur);
+            light(L1,dur);
+            light(L0,dur);
+        }
     }
     if (pattern == 2) {
         for (int i = 0; i < reps; i ++) {
-            light(L3,300);
-            light(L0,300);
-            light(L2,300);
-            light(L1,300);
+            light(L3,dur);
+            light(L0,dur);
+            light(L2,dur);
+            light(L1,dur);
         }
     }
     return;
